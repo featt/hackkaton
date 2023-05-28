eeac84421cc34c3b9fe5e7e2a7e374aa126902e0
featt <kachalo55@gmail.com>
2023-05-29 00:59:03 +0300

    p

--- front/src/pages/dashboard/profile.jsx
+++ front/src/pages/dashboard/profile.jsx
@@ -28,8 +28,7 @@ export function Profile() {
   console.log(user);
   return (
     <>
-      <div className="relative mt-8 h-72 w-full overflow-hidden rounded-xl bg-[url(https://images.unsplash.com/photo-1531512073830-ba890ca4eba2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1920&q=80)] bg-cover	bg-center">
-        <div className="absolute inset-0 h-full w-full bg-blue-500/50" />
+      <div className="relative mt-8 h-10 w-full overflow-hidden rounded-xl  bg-cover	bg-center">
       </div>
       <Card className="mx-3 -mt-16 mb-6 lg:mx-4">
         <CardBody className="p-4">
@@ -44,59 +43,28 @@ export function Profile() {
             <div className="w-96">
               <Tabs value="app">
                 <TabsHeader>
+                <Tab value="settings">
+                    <Cog6ToothIcon className="-mt-1 mr-2 inline-block h-5 w-5" />
+                    Баллы: 230
+                  </Tab>
                   <Tab value="app">
                     <HomeIcon className="-mt-1 mr-2 inline-block h-5 w-5" />
-                    App
-                  </Tab>
-                  <Tab value="message">
-                    <ChatBubbleLeftEllipsisIcon className="-mt-0.5 mr-2 inline-block h-5 w-5" />
-                    Message
-                  </Tab>
-                  <Tab value="settings">
-                    <Cog6ToothIcon className="-mt-1 mr-2 inline-block h-5 w-5" />
-                    Settings
+                    Обменять
                   </Tab>
                 </TabsHeader>
               </Tabs>
             </div>
           </div>
-          <div className="gird-cols-1 mb-12 grid gap-12 px-4 lg:grid-cols-2 xl:grid-cols-3">
-            <div>
-              <Typography variant="h6" color="blue-gray" className="mb-3">
-                Platform Settings
-              </Typography>
-              <div className="flex flex-col gap-12">
-                {platformSettingsData.map(({ title, options }) => (
-                  <div key={title}>
-                    <Typography className="mb-4 block text-xs font-semibold uppercase text-blue-gray-500">
-                      {title}
-                    </Typography>
-                    <div className="flex flex-col gap-6">
-                      {options.map(({ checked, label }) => (
-                        <Switch
-                          key={label}
-                          id={label}
-                          label={label}
-                          defaultChecked={checked}
-                          labelProps={{
-                            className: "text-sm font-normal text-blue-gray-500",
-                          }}
-                        />
-                      ))}
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </div>
+          <div className="gird-cols-1 mb-12 grid gap-12 px-4 lg:grid-cols-1 xl:grid-cols-2">
             <ProfileInfoCard
-              title="Profile Information"
-              description="Hi, I'm Alec Thompson, Decisions: If you can't decide, the answer is no. If two equally difficult paths, choose the one more painful in the short term (pain avoidance is creating an illusion of equality)."
+              title="Информация о профиле"
+              description="Привет, я Степан Плеханов. Решения: Если не можете принять решение, ответ - нет. Если перед вами стоят два одинаково сложных пути, выберите тот, который более болезненен в краткосрочной перспективе (избегание боли создает иллюзию равенства)."
               details={{
-                "first name": "Alec M. Thompson",
-                mobile: "(44) 123 1234 123",
-                email: "alecthompson@mail.com",
-                location: "USA",
-                social: (
+                "Имя": "Плеханов Степан",
+                "Телефон": "+7 (908) 859 72 32",
+                "email": "crayon-d@mail.ru",
+                "Локация": "Москва, Россия",
+                "Соцсети": (
                   <div className="flex items-center gap-4">
                     <i className="fa-brands fa-facebook text-blue-700" />
                     <i className="fa-brands fa-twitter text-blue-400" />
@@ -112,7 +80,7 @@ export function Profile() {
             />
             <div>
               <Typography variant="h6" color="blue-gray" className="mb-3">
-                Platform Settings
+                RoadMap до стажировки
               </Typography>
               <ul className="flex flex-col gap-6">
                 {conversationsData.map((props) => (
@@ -121,7 +89,7 @@ export function Profile() {
                     {...props}
                     action={
                       <Button variant="text" size="sm">
-                        reply
+                        Начать
                       </Button>
                     }
                   />
@@ -131,29 +99,24 @@ export function Profile() {
           </div>
           <div className="px-4 pb-4">
             <Typography variant="h6" color="blue-gray" className="mb-2">
-              Projects
+              Стажировки
             </Typography>
             <Typography
               variant="small"
               className="font-normal text-blue-gray-500"
             >
-              Architects design houses
+              Твои приглашения на стажировку
             </Typography>
             <div className="mt-6 grid grid-cols-1 gap-12 md:grid-cols-2 xl:grid-cols-4">
               {projectsData.map(
                 ({ img, title, description, tag, route, members }) => (
                   <Card key={title} color="transparent" shadow={false}>
                     <CardHeader
-                      floated={false}
-                      color="gray"
-                      className="mx-0 mt-0 mb-4 h-64 xl:h-40"
+                      className="mx-0 mt-0 mb-4"
                     >
-                      <img
-                        src={img}
-                        alt={title}
-                        className="h-full w-full object-cover"
-                      />
+
                     </CardHeader>
+                    
                     <CardBody className="py-0 px-1">
                       <Typography
                         variant="small"
@@ -168,6 +131,11 @@ export function Profile() {
                       >
                         {title}
                       </Typography>
+                      <Link to={route}>
+                        <Button variant="outlined" size="sm">
+                          Принять
+                        </Button>
+                      </Link>
                       <Typography
                         variant="small"
                         className="font-normal text-blue-gray-500"
@@ -176,11 +144,6 @@ export function Profile() {
                       </Typography>
                     </CardBody>
                     <CardFooter className="mt-6 flex items-center justify-between py-0 px-1">
-                      <Link to={route}>
-                        <Button variant="outlined" size="sm">
-                          view project
-                        </Button>
-                      </Link>
                       <div>
                         {members.map(({ img, name }, key) => (
                           <Tooltip key={name} content={name}>
